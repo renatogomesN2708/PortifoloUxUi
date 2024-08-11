c86beff2d2d3cfc87e10c3f654f4b69051220f55
renatogomesN2708 <wellenvitoria.1904@gmail.com>
2024-08-11 14:43:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function  buttonfunction() {
+    let s = document.getElementById("saiba");
+    let menu = document.getElementById('sobre-oculto');
+    menu.style.opacity = '100%'
+    let ap = document.getElementById("apresentation");
+    ap.style.opacity = '5%'
+    let proje = document.getElementById("section");
+    let proj = document.getElementById("h");
+    proje.style.opacity = '5%'
+    proj.style.opacity = '5%'
+}
